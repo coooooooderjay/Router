a700d319554252fbdde918b79328a7aef9ab5b21
Jay <coooooooderjay@163.com>
2021-10-09 15:35:05 +0800

    Initial commit

---
+++ Example/Router/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Router
+//
+//  Created by coooooooderjay on 10/09/2021.
+//  Copyright (c) 2021 coooooooderjay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
